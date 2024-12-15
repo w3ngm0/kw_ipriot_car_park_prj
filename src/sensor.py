9e567737cf248b4ce198e5af0770bbc682c507f2
w3ngm0 <waggmo@gmail.com>
2024-12-15 18:25:09 +0800

    fix: implement __str__ method and replace multiline strings with comments

--- src/sensor.py
+++ src/sensor.py
@@ -7,14 +7,24 @@ class Sensor(ABC):
     Constructor method for Sensor class. Inherits from ABC abstract method
     """
     def __init__(self, id, is_active, car_park):
+        """
+        Initialize the Sensor object
+
+        Args:
+            id (str): Unique identifier for the sensor.
+            is_active (bool): Sensor's active status.
+            car_park (CarPark): Reference to the associated car park.
+        """
         self.id = id
         self.is_active = is_active
         self.car_park = car_park
 
     def __str__(self):
-        ...  # Return string containing sensor's id and status
+        """
+        Return string containing sensor's id
+        """
+        return f"Sensor {self.id}"
 
-    """need subclass Entry and Exit for sensor"""
 
     @abstractmethod
     def update_car_park(self, plate):
@@ -30,7 +40,7 @@ class Sensor(ABC):
 
 
 class EntrySensor(Sensor):
-    """ A sensor for detecting vehicles existing in the car park"""
+    """ A sensor for detecting vehicles entering the car park"""
     def update_car_park(self, plate):
         if self.is_active:
             print(f"Incoming 🚘 vehicle detected. Plate: {plate}")
@@ -40,7 +50,7 @@ class EntrySensor(Sensor):
 
 
 class ExitSensor(Sensor):
-    """ Remove a vehicle from the car park """
+    """ A sensor for detecting vehicles exiting the car park"""
     def update_car_park(self, plate):
         if self.is_active:
             print(f"Outgoing 🚗 vehicle detected. Plate: {plate}")
@@ -50,3 +60,4 @@ class ExitSensor(Sensor):
 
     def _scan_plate(self):
         return random.choice(self.car_park.plates)
+
